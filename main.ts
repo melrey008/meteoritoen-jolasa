aac38fb08764e9485e255828592ab2f823cf92a9
melrey008 <101406273+melrey008@users.noreply.github.com>
2022-05-20 14:12:16 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,23 @@
 input.onButtonPressed(Button.A, function () {
-	
-})
-input.onButtonPressed(Button.AB, function () {
-	
+    jokalari.change(LedSpriteProperty.X, -1)
 })
 input.onButtonPressed(Button.B, function () {
-	
+    jokalari.change(LedSpriteProperty.X, 1)
 })
-let jokalari = game.createSprite(2, 4)
+let meteorito: game.LedSprite = null
+let jokalari: game.LedSprite = null
+jokalari = game.createSprite(2, 4)
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        jokalari.change(LedSpriteProperty.X, 1)
+    basic.pause(randint(1000, 1500))
+    meteorito = game.createSprite(randint(1, 4), 1)
+    for (let index = 0; index < 4; index++) {
+        basic.pause(200)
+        meteorito.change(LedSpriteProperty.Y, 1)
     }
-    if (input.buttonIsPressed(Button.B)) {
-        jokalari.change(LedSpriteProperty.X, -1)
-    }
-    if (input.buttonIsPressed(Button.AB)) {
-    	
+    if (meteorito.isTouching(jokalari)) {
+        game.gameOver()
+    } else {
+        game.addScore(1)
+        meteorito.delete()
     }
 })
